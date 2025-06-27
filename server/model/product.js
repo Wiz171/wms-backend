79dbe9bdc2175026e4967cac3c94a494bac54809
Wiz171 <thitsazaw272@gmail.com>
2025-06-27 05:01:25 +0000

    edit product test 2

--- server/model/product.js
+++ server/model/product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: String,
     price: { type: Number, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     createdAt: { type: Date, default: Date.now },
     specs: {
         type: Object, // Flexible for dynamic specs (key-value pairs)
